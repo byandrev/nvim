645c79d736b3940daedfa4e025c550b8a835013b
Andres Parra <byandrev@gmail.com>
2024-02-07 20:39:56 -0500

    install lsp, cmp and mason

--- lua/config/basics.lua
+++ lua/config/basics.lua
@@ -2,8 +2,26 @@ vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.syntax = "enable"
+vim.opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
+
+vim.opt.wrap = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.cmdheight = 0
+vim.opt.colorcolumn = "80"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.errorbells = false
+vim.o.splitbelow = true -- automagically h split going down
+vim.o.splitright = true -- automagically v split going right
+vim.opt.backspace = { "eol", "start", "indent" }
+
+-- search
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.smartcase = true -- smart case
+vim.opt.wildignore = vim.opt.wildignore + { "*/node_modules/*", "*/.git/*", "*/vendor/*" }
+vim.opt.wildmenu = true -- make tab completion for files/buffers act like bash
 
 -- Backups
 vim.opt.backup = false
 vim.opt.swapfile = false
-vim.opt.writebackup = false 
+vim.opt.writebackup = false
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,29 +1,29 @@
 return {
 	{
-	"nvim-neo-tree/neo-tree.nvim",
+		"nvim-neo-tree/neo-tree.nvim",
 
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
 
-	keys = {
-		{ "<leader>t", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-	},
+		keys = {
+			{ "<leader>t", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+		},
 
-	config = function()
-		require("neo-tree").setup({
-			popup_border_style = "rounded",
-			enable_git_status = true,
-			enable_diagnostics = true,
+		config = function()
+			require("neo-tree").setup({
+				popup_border_style = "rounded",
+				enable_git_status = true,
+				enable_diagnostics = true,
 
-			window = {
-				width = 30,
-			},
-		})
+				window = {
+					width = 30,
+				},
+			})
 
-		vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-	end,
-}
+			vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+		end,
+	}
 }
